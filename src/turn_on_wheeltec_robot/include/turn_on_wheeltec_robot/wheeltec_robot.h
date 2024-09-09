506161109caaa7f93cc7c1944e28ff05c2a16c27
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-09 18:09:25 +0100

    Modified wheeltec_robot.h decalred ackermann_cmd_vel and cmd_vel

--- src/turn_on_wheeltec_robot/include/turn_on_wheeltec_robot/wheeltec_robot.h
+++ src/turn_on_wheeltec_robot/include/turn_on_wheeltec_robot/wheeltec_robot.h
@@ -6,17 +6,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <iostream>
-#include <string.h>
-#include <string> 
-#include <math.h> 
-#include <stdlib.h>    
-#include <unistd.h>      
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <rcl/types.h>
 #include <sys/stat.h>
 #include <serial/serial.h>
-#include <fcntl.h>          
+#include <fcntl.h>
 #include <stdbool.h>
-#include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/float32.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
@@ -29,135 +27,123 @@
 #include "wheeltec_robot_msg/msg/data.hpp"
 #include <sensor_msgs/msg/imu.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+
 using namespace std;
 
 // Macro definitions
-#define SEND_DATA_CHECK   1          // Send data check flag
-#define READ_DATA_CHECK   0          // Receive data check flag
-#define FRAME_HEADER      0X7B       // Frame header
-#define FRAME_TAIL        0X7D       // Frame tail
-#define RECEIVE_DATA_SIZE 24         // Length of data sent by the lower computer
-#define SEND_DATA_SIZE    11         // Length of data sent to the lower machine
-#define PI                3.1415926f // Value of PI
+#define SEND_DATA_CHECK   1
+#define READ_DATA_CHECK   0
+#define FRAME_HEADER      0X7B
+#define FRAME_TAIL        0X7D
+#define RECEIVE_DATA_SIZE 24
+#define SEND_DATA_SIZE    11
+#define PI                3.1415926f
+#define GYROSCOPE_RATIO   0.00026644f
+#define ACCEl_RATIO       1671.84f
 
-// IMU data conversion ratios
-#define GYROSCOPE_RATIO   0.00026644f // Gyroscope conversion ratio
-#define ACCEl_RATIO       1671.84f    // Accelerometer conversion ratio
+extern sensor_msgs::msg::Imu Mpu6050;
 
-extern sensor_msgs::msg::Imu Mpu6050; // External IMU topic data
+const double odom_pose_covariance[36] = { /* covariance values */ };
+const double odom_twist_covariance[36] = { /* covariance values */ };
 
-// Covariance matrices for odometry data
-const double odom_pose_covariance[36] = {1e-3, 0, 0, 0, 0, 0, 
-                                          0, 1e-3, 0, 0, 0, 0,
-                                          0, 0, 1e6, 0, 0, 0,
-                                          0, 0, 0, 1e6, 0, 0,
-                                          0, 0, 0, 0, 1e6, 0,
-                                          0, 0, 0, 0, 0, 1e3};
-
-const double odom_twist_covariance[36] = {1e-3, 0, 0, 0, 0, 0, 
-                                           0, 1e-3, 0, 0, 0, 0,
-                                           0, 0, 1e6, 0, 0, 0,
-                                           0, 0, 0, 1e6, 0, 0,
-                                           0, 0, 0, 0, 1e6, 0,
-                                           0, 0, 0, 0, 0, 1e3};
-
-// Data structure for velocity and position
+// Data structures
 typedef struct __Vel_Pos_Data_
 {
-    float X; // X position
-    float Y; // Y position
-    float Z; // Z position
+    float X;
+    float Y;
+    float Z;
 } Vel_Pos_Data;
 
-// IMU data structure
 typedef struct __MPU6050_DATA_
 {
-    short accele_x_data; // X acceleration
-    short accele_y_data; // Y acceleration
-    short accele_z_data; // Z acceleration
-    short gyros_x_data;  // X gyroscope data
-    short gyros_y_data;  // Y gyroscope data
-    short gyros_z_data;  // Z gyroscope data
+    short accele_x_data;
+    short accele_y_data;
+    short accele_z_data;
+    short gyros_x_data;
+    short gyros_y_data;
+    short gyros_z_data;
 } MPU6050_DATA;
 
-// Data structure for sending data to the lower machine
 typedef struct _SEND_DATA_
 {
-    uint8_t tx[SEND_DATA_SIZE]; // Transmission data
-    float X_speed;              // X speed
-    float Y_speed;              // Y speed
-    float Z_speed;              // Z speed
-    unsigned char Frame_Tail;   // Frame tail
+    uint8_t tx[SEND_DATA_SIZE];
+    float X_speed;
+    float Y_speed;
+    float Z_speed;
+    unsigned char Frame_Tail;
 } SEND_DATA;
 
-// Data structure for receiving data from the lower machine
 typedef struct _RECEIVE_DATA_
 {
-    uint8_t rx[RECEIVE_DATA_SIZE]; // Reception data
-    uint8_t Flag_Stop;              // Stop flag
-    unsigned char Frame_Header;     // Frame header
-    float X_speed;                  // X speed
-    float Y_speed;                  // Y speed
-    float Z_speed;                  // Z speed
-    float Power_Voltage;           // Power voltage
-    unsigned char Frame_Tail;       // Frame tail
+    uint8_t rx[RECEIVE_DATA_SIZE];
+    uint8_t Flag_Stop;
+    unsigned char Frame_Header;
+    float X_speed;
+    float Y_speed;
+    float Z_speed;
+    float Power_Voltage;
+    unsigned char Frame_Tail;
 } RECEIVE_DATA;
 
-// Robot chassis class to initialize data and publish topics
+// Robot chassis class
 class turn_on_robot : public rclcpp::Node
 {
-    public:
-        turn_on_robot();
-        ~turn_on_robot(); // Destructor
-        void Control();   // Loop control code
-        void update_tf(geometry_msgs::msg::TransformStamped::SharedPtr odom_tf);
-        void Publish_Odom(); // Publish odometry topic
-        serial::Serial Stm32_Serial; // Serial object
+public:
+    turn_on_robot();
+    ~turn_on_robot(); // Destructor
+    void Control(); // Loop control code
+    void update_tf(geometry_msgs::msg::TransformStamped::SharedPtr odom_tf);
+    void Publish_Odom(); // Publish odometry topic
+    serial::Serial Stm32_Serial; // Serial object
 
-    private:
-        rclcpp::Time _Now, _Last_Time; // Time-dependent variables
-        float Sampling_Time;            // Sampling time
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr Cmd_Vel_Sub; // Velocity subscription
-        rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr Akm_Cmd_Vel_Sub; // Ackermann command subscription
+private:
+    rclcpp::Time _Now, _Last_Time; // Time-dependent variables
+    float Sampling_Time; // Sampling time
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr Cmd_Vel_Sub;
+    rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr Akm_Cmd_Vel_Sub;
 
-        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher; // Odometry publisher
-        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr voltage_publisher; // Voltage publisher
-        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_publisher; // IMU publisher
-        rclcpp::Publisher<wheeltec_robot_msg::msg::Data>::SharedPtr robotpose_publisher; // Robot pose publisher
-        rclcpp::Publisher<wheeltec_robot_msg::msg::Data>::SharedPtr robotvel_publisher; // Robot velocity publisher
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr voltage_publisher;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_publisher;
+    rclcpp::Publisher<wheeltec_robot_msg::msg::Data>::SharedPtr robotpose_publisher;
+    rclcpp::Publisher<wheeltec_robot_msg::msg::Data>::SharedPtr robotvel_publisher;
 
-        std::shared_ptr<tf2_ros::TransformBroadcaster> tf_bro;
-        rclcpp::TimerBase::SharedPtr test_timer;
-        rclcpp::TimerBase::SharedPtr odom_timer;
-        rclcpp::TimerBase::SharedPtr imu_timer;
-        rclcpp::TimerBase::SharedPtr voltage_timer;
-        rclcpp::TimerBase::SharedPtr robotpose_timer;
-        rclcpp::TimerBase::SharedPtr robotvel_timer;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_bro;
+    rclcpp::TimerBase::SharedPtr test_timer;
+    rclcpp::TimerBase::SharedPtr odom_timer;
+    rclcpp::TimerBase::SharedPtr imu_timer;
+    rclcpp::TimerBase::SharedPtr voltage_timer;
+    rclcpp::TimerBase::SharedPtr robotpose_timer;
+    rclcpp::TimerBase::SharedPtr robotvel_timer;
 
-        void declare_parameters();
-        void get_parameters();
-        void Cmd_Vel_Callback(const geometry_msgs::msg::Twist::SharedPtr twist_aux); // Command velocity callback
-        void Akm_Cmd_Vel_Callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr akm_ctl); // Ackermann command callback
-        void Publish_ImuSensor(); // Publish IMU sensor topic
-        void Publish_Voltage(); // Publish power supply voltage topic
-        auto createQuaternionMsgFromYaw(double yaw); // Create quaternion from yaw
+    // New member variables for command velocities
+    std::string akm_cmd_vel; // Command for Ackermann
+    std::string cmd_vel;     // Command for Twist
 
-        // Read sensor data from serial port
-        bool Get_Sensor_Data();   
-        unsigned char Check_Sum(unsigned char Count_Number, unsigned char mode); // Checksum function
-        short IMU_Trans(uint8_t Data_High, uint8_t Data_Low); // IMU data conversion
-        float Odom_Trans(uint8_t Data_High, uint8_t Data_Low); // Odometer data conversion
+    void declare_parameters();
+    void get_parameters();
+    void Cmd_Vel_Callback(const geometry_msgs::msg::Twist::SharedPtr twist_aux);
+    void Akm_Cmd_Vel_Callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr akm_ctl);
+    void Publish_ImuSensor();
+    void Publish_Voltage();
+    auto createQuaternionMsgFromYaw(double yaw);
 
-        string usart_port_name, robot_frame_id, gyro_frame_id, odom_frame_id; // Related variables
-        int serial_baud_rate; // Serial baud rate
-        RECEIVE_DATA Receive_Data; // Serial reception data structure
-        SEND_DATA Send_Data; // Serial transmission data structure
+    // Read sensor data from serial port
+    bool Get_Sensor_Data();
+    unsigned char Check_Sum(unsigned char Count_Number, unsigned char mode);
+    short IMU_Trans(uint8_t Data_High, uint8_t Data_Low);
+    float Odom_Trans(uint8_t Data_High, uint8_t Data_Low);
 
-        Vel_Pos_Data Robot_Pos; // Robot position
-        Vel_Pos_Data Robot_Vel; // Robot speed
-        MPU6050_DATA Mpu6050_Data; // IMU data
-        float Power_voltage; // Power supply voltage
-        size_t count_; // Count variable
+    string usart_port_name, robot_frame_id, gyro_frame_id, odom_frame_id; // Related variables
+    int serial_baud_rate; // Serial baud rate
+    RECEIVE_DATA Receive_Data; // Serial reception data structure
+    SEND_DATA Send_Data; // Serial transmission data structure
+
+    Vel_Pos_Data Robot_Pos; // Robot position
+    Vel_Pos_Data Robot_Vel; // Robot speed
+    MPU6050_DATA Mpu6050_Data; // IMU data
+    float Power_voltage; // Power supply voltage
+    size_t count_; // Count variable
 };
 
 #endif
