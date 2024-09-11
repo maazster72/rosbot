141c7d1d82b49629e109bd61c65edfe38c3a7e6f
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-11 11:14:53 +0100

    Modified guidance_interface.hpp to fix build errors

--- src/guidance_interface/include/guidance_interface/guidance_interface.hpp
+++ src/guidance_interface/include/guidance_interface/guidance_interface.hpp
@@ -7,18 +7,16 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-
 #include "nav2_core/global_planner.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "nav2_util/robot_utils.hpp"
 #include "nav2_util/lifecycle_node.hpp"
 #include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "tf2_ros/buffer.h" // Include tf2_ros for tf buffer
 
-namespace guidance_interface
-{
+namespace guidance_interface {
 
-class GuidanceInterface : public nav2_core::GlobalPlanner
-{
+class GuidanceInterface : public nav2_core::GlobalPlanner {
 public:
   GuidanceInterface() = default;
   ~GuidanceInterface() = default; 
@@ -26,7 +24,8 @@ public:
   // Plugin configure
   void configure(
     const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
-    std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+    const std::string & name,  // Pass name as const reference for better performance
+    std::shared_ptr<tf2_ros::Buffer> tf,
     std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
   // Plugin cleanup
@@ -45,18 +44,19 @@ public:
 
 private:
   // TF buffer
-  std::shared_ptr<tf2_ros::Buffer> tf_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;  // Ensure the tf buffer is a shared pointer
 
   // Node pointer
-  nav2_util::LifecycleNode::SharedPtr node_;
+  rclcpp_lifecycle::LifecycleNode::SharedPtr node_;  // Changed to SharedPtr
 
   // Global Costmap
-  nav2_costmap_2d::Costmap2D * costmap_;
+  nav2_costmap_2d::Costmap2D * costmap_;  // Pointer to Costmap2D
 
   // The global frame of the costmap
-  std::string global_frame_, name_;
+  std::string global_frame_;  // Make sure to have only the global frame variable
+  std::string name_;  // Name of the planner
 
-  double interpolation_resolution_;
+  double interpolation_resolution_;  // Resolution for interpolation
 };
 
 }  // namespace guidance_interface
