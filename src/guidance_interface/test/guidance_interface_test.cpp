4ceec222d7d6944d4c7890eee8172d0c091153a1
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-13 14:07:35 +0100

    Removed tests in test file

--- src/guidance_interface/test/guidance_interface_test.cpp
+++ src/guidance_interface/test/guidance_interface_test.cpp
@@ -37,41 +37,7 @@ protected:
   }
 };
 
-TEST_F(GuidanceInterfaceTest, TestConfigure) {
-  // Ensure the planner is correctly configured
-  EXPECT_EQ(planner_->getGlobalFrameID(), costmap_ros_->getGlobalFrameID());
-}
-
-TEST_F(GuidanceInterfaceTest, TestCreatePlan) {
-  // Create start and goal poses
-  geometry_msgs::msg::PoseStamped start, goal;
-  start.header.frame_id = costmap_ros_->getGlobalFrameID();
-  goal.header.frame_id = costmap_ros_->getGlobalFrameID();
-  start.pose.position.x = 0.0;
-  start.pose.position.y = 0.0;
-  goal.pose.position.x = 1.0;
-  goal.pose.position.y = 1.0;
-
-  // Generate the plan
-  nav_msgs::msg::Path path = planner_->createPlan(start, goal);
-
-  // Validate that the path is non-empty and contains the correct number of points
-  EXPECT_FALSE(path.poses.empty());
-  EXPECT_EQ(path.poses.size(), 11); // Check the expected number of interpolated poses + goal pose
-}
-
-TEST_F(GuidanceInterfaceTest, TestInvalidFrame) {
-  // Create start and goal poses with incorrect frame IDs
-  geometry_msgs::msg::PoseStamped start, goal;
-  start.header.frame_id = "invalid_frame";
-  goal.header.frame_id = costmap_ros_->getGlobalFrameID();
-
-  // Generate the plan with an invalid start frame
-  nav_msgs::msg::Path path = planner_->createPlan(start, goal);
-
-  // Validate that the path is empty due to the invalid frame
-  EXPECT_TRUE(path.poses.empty());
-}
+// Add tests here
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
