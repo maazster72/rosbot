3aa22846732acceae8f327a7734d11539febd46f
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-23 14:53:36 +0100

    Add controller.hpp

---
+++ src/rosbot_vms/vms_core/include/vms_core/controller.hpp
@@ -0,0 +1,88 @@
+#ifndef NAV2_CORE__CONTROLLER_HPP_
+#define NAV2_CORE__CONTROLLER_HPP_
+
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "nav_msgs/msg/path.hpp"
+
+
+namespace vms_core
+{
+
+/**
+ * @class Controller
+ * @brief controller interface that acts as a virtual base class for all controller plugins
+ */
+class Controller
+{
+public:
+  using Ptr = std::shared_ptr<vms_core::Controller>;
+
+
+  /**
+   * @brief Virtual destructor
+   */
+  virtual ~Controller() {}
+
+  /**
+   * @param  parent pointer to user's node
+   */
+  virtual void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr &) = 0;
+
+  /**
+   * @brief Method to cleanup resources.
+   */
+  virtual void cleanup() = 0;
+
+  /**
+   * @brief Method to active planner and any threads involved in execution.
+   */
+  virtual void activate() = 0;
+
+  /**
+   * @brief Method to deactive planner and any threads involved in execution.
+   */
+  virtual void deactivate() = 0;
+
+  /**
+   * @brief local setPlan - Sets the global plan
+   * @param path The global plan
+   */
+  virtual void setPlan(const nav_msgs::msg::Path & path) = 0;
+
+  /**
+   * @brief Controller computeVelocityCommands - calculates the best command given the current pose and velocity
+   *
+   * It is presumed that the global plan is already set.
+   *
+   * This is mostly a wrapper for the protected computeVelocityCommands
+   * function which has additional debugging info.
+   *
+   * @param pose Current robot pose
+   * @param velocity Current robot velocity
+   * @return The best command for the robot to drive
+   */
+  virtual geometry_msgs::msg::TwistStamped computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped & pose,
+    const geometry_msgs::msg::Twist & velocity) = 0;
+
+  /**
+   * @brief Limits the maximum linear speed of the robot.
+   * @param speed_limit expressed in absolute value (in m/s)
+   * or in percentage from maximum robot speed.
+   * @param percentage Setting speed limit in percentage if true
+   * or in absolute values in false case.
+   */
+  virtual void setSpeedLimit(const double & speed_limit, const bool & percentage) = 0;
+};
+
+}  // namespace nav2_core
+
+#endif  // NAV2_CORE__CONTROLLER_HPP_
