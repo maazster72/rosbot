3e3011426e0d73e8f008f3356194f675d77b8d27
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-23 10:40:13 +0100

    Run vms_guidance_interface with no translator

--- src/rosbot_vms/vms_guidance_interface/src/main.cpp
+++ src/rosbot_vms/vms_guidance_interface/src/main.cpp
@@ -3,13 +3,28 @@
 #include "vms_guidance_interface/guidance_interface.hpp"
 #include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char ** argv)
-{
-  rclcpp::init(argc, argv);
-  auto options = rclcpp::NodeOptions();
-  auto node = std::make_shared<vms_guidance_interface::GuidanceInterface>(options);
-  rclcpp::spin(node->get_node_base_interface());
-  rclcpp::shutdown();
+#include "rclcpp/rclcpp.hpp"
+#include "vms_guidance_interface/guidance_interface.hpp"
 
-  return 0;
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+
+    // Create a shared pointer to the node
+    auto node = std::make_shared<vms_guidance_interface::GuidanceInterface>(rclcpp::NodeOptions());
+
+    // Configure the node
+    auto configure_result = node->configure();
+
+    // Activate the node
+    auto activate_result = node->activate();
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Guidance Interface is now active and ready to accept goals.");
+
+    // Spin the node to keep it alive and process callbacks
+    rclcpp::spin(node->get_node_base_interface());
+
+    // Shutdown and cleanup
+    rclcpp::shutdown();
+    return 0;
 }
