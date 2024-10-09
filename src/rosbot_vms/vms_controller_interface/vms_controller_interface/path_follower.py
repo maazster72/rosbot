c6d680803f87a4bb6d17c63390abc967c4ea2404
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-10-09 12:21:07 +0100

    navigating through multiple poses successfully

--- src/rosbot_vms/vms_controller_interface/vms_controller_interface/path_follower.py
+++ src/rosbot_vms/vms_controller_interface/vms_controller_interface/path_follower.py
@@ -15,12 +15,11 @@ class PathFollower(Node):
         super().__init__('path_follower')
 
         self.plan = None
-        self.poses = None
         self.current_pose = PoseStamped()
-        self.current_goal_pose_index = None
-        self.goal_pose = None
-        self.distance_to_goal = None
-        self.threshold_linear = 0.08
+        self.current_goal_pose_index = 0
+        self.goal_pose = PoseStamped()
+        self.distance_to_goal = float("inf")
+        self.threshold_linear = 0.1
 
         # Subscribe to the /plan topic
         self.plan_subscriber = self.create_subscription(
@@ -55,59 +54,27 @@ class PathFollower(Node):
     def goal_callback(self, msg: PoseStamped):
         self.goal_pose = msg
         self.distance_to_goal = util.get_distance_to_target_pose(self.current_pose, self.goal_pose)
+        # Logging
+        self.get_logger().info(f"New goal pose received: {self.goal_pose}")
 
     def plan_callback(self, msg: Path):
-        self.plan = msg
         self.current_goal_pose_index = 0
-        self.goal_pose_publisher.publish(self.plan.poses[self.current_goal_pose_index])
-        
+        self.goal_pose_publisher.publish(msg.poses[self.current_goal_pose_index])
+        self.plan = msg
         # Logging
-        self.get_logger().info(f"Received path: {self.plan}")
+        self.get_logger().info(f"New path received: {self.plan}")
 
     def odom_callback(self, msg: Odometry):
         self.current_pose.pose.position = msg.pose.pose.position
         self.current_pose.pose.orientation = msg.pose.pose.orientation
     
     def move_towards_goal(self):
-        # if self.plan:
-        #     # Logging
-        #     # self.get_logger().info(f"Current target pose ({self.current_target_pose_index + 1}/{len(self.poses) + 1}): {self.current_target_pose}")
-        #     self.get_logger().info(f"Distance to target pose: {self.distance_to_target}")
-        #     # self.get_logger().info(f"Distance to goal pose: {self.distance_to_goal}")
-        #     # Check if goal has been reached
-        #     if abs(self.distance_to_goal) < self.threshold_linear:
-        #         self.plan = None
-        #         self.poses = None
-        #         self.current_pose = PoseStamped()
-        #         self.current_target_pose_index = None
-        #         self.current_target_pose = None
-        #         self.distance_to_target = None
-        #         self.goal_pose = None
-        #         self.goal_achieved = True
-        #         self.distance_to_goal = None
-        #         # Logging
-        #         self.get_logger().info(f"Goal pose reached. Executed plan successfully")
-        #     # Check if target pose has been reached
-        #     elif abs(self.distance_to_target) < self.threshold_linear:
-        #         # Logging
-        #         self.get_logger().info(f"Pose {self.current_target_pose_index + 1}/{len(self.poses)} complete.")
-        #         # Check if any more poses to follow
-        #         if len(self.poses) > 0:
-        #             self.current_target_pose_index += 1
-        #             self.current_target_pose = self.poses[self.current_target_pose_index]
-        #             self.distance_to_target = util.get_distance_to_target_pose(self.current_pose, self.current_target_pose)
-        #             # Logging
-        #             self.get_logger().info(f"New target pose: {self.current_target_pose}")
-        #     else:
-        #         # Compute cmd_vel
-        #         cmd_vel = controller.move_to_target(self.current_pose, self.current_target_pose)
-        #         # Logging
-        #         self.get_logger().info(f"cmd_vel: {cmd_vel}")
-            
-        # # Update distances
-        # self.distance_to_target = util.get_distance_to_target_pose(self.current_pose, self.current_target_pose)
-        # self.distance_to_goal = util.get_distance_to_target_pose(self.current_pose, self.goal_pose)
-        if self.goal_pose:
+        if self.plan:
+            self.get_logger().info(f"Current Goal: {self.goal_pose}")
+            self.get_logger().info(f"Current pose (Odomoetry): {self.current_pose}")
+            self.get_logger().info(f"Distance to current goal: {self.distance_to_goal}")
+            self.navThroughPoses()
+        elif self.goal_pose:
             # Logging
             self.get_logger().info(f"Goal: {self.goal_pose}")
             self.get_logger().info(f"Current pose (Odomoetry): {self.current_pose}")
@@ -123,89 +90,70 @@ class PathFollower(Node):
             # Logging
             self.get_logger().info(f"Goal pose reached. Executed plan successfully")
         else:
-            # Compute cmd_vel
-            cmd_vel = controller.move_to_target(self.current_pose, self.goal_pose)
+            self.compute_cmd_velocity()
+
+    def navThroughPoses(self):
+        if abs(self.distance_to_goal) < self.threshold_linear:
             # Logging
-            self.get_logger().info(f"Moving with cmd_vel: {cmd_vel}")
-            # Publish cmd_vel
-            self.cmd_vel_publisher.publish(cmd_vel)
-            # Update distance
-            self.distance_to_goal = util.get_distance_to_target_pose(self.current_pose, self.goal_pose)
+            self.get_logger().info(f"Goal pose reached. ({self.current_goal_pose_index + 1}/{len(self.plan.poses)})")
+            if self.current_goal_pose_index != (len(self.plan.poses) - 1):
+                self.current_goal_pose_index += 1
+                self.goal_pose_publisher.publish(self.plan.poses[self.current_goal_pose_index])
+            else:
+                self.plan = None
+                self.current_goal_pose_index = None
+                self.goal_pose = None
+                self.distance_to_goal = None
+                # Logging
+                self.get_logger().info(f"Goal pose reached. Executed plan successfully")
+        else:
+            self.compute_cmd_velocity()
 
-    # def smooth_nav_path(self, input_path, num_points=100):        
-    #     # Extract x and y positions from the input path
-    #     x_points = [pose.pose.position.x for pose in input_path.poses]
-    #     y_points = [pose.pose.position.y for pose in input_path.poses]
+    def compute_cmd_velocity(self):
+        # Compute cmd_vel
+        cmd_vel = controller.move_to_target(self.current_pose, self.goal_pose)
+        # Logging
+        self.get_logger().info(f"Moving with cmd_vel: {cmd_vel}")
+        # Publish cmd_vel
+        self.cmd_vel_publisher.publish(cmd_vel)
+        # Update distance
+        self.distance_to_goal = util.get_distance_to_target_pose(self.current_pose, self.goal_pose)
 
-    #     # Ensure there are enough points to interpolate
-    #     if len(x_points) < 3 or len(y_points) < 3:
-    #         raise ValueError("Not enough points to perform spline interpolation")
+    def smooth_nav_path(self, input_path, num_points=100):        
+        # Extract x and y positions from the input path
+        x_points = [pose.pose.position.x for pose in input_path.poses]
+        y_points = [pose.pose.position.y for pose in input_path.poses]
+
+        # Ensure there are enough points to interpolate
+        if len(x_points) < 3 or len(y_points) < 3:
+            raise ValueError("Not enough points to perform spline interpolation")
         
-    #     # Perform cubic spline interpolation
-    #     t = np.arange(len(x_points))  # Time (or index) array for the original waypoints
-    #     cs_x = CubicSpline(t, x_points)  # Spline for x-coordinates
-    #     cs_y = CubicSpline(t, y_points)  # Spline for y-coordinates
+        # Perform cubic spline interpolation
+        t = np.arange(len(x_points))  # Time (or index) array for the original waypoints
+        cs_x = CubicSpline(t, x_points)  # Spline for x-coordinates
+        cs_y = CubicSpline(t, y_points)  # Spline for y-coordinates
         
-    #     # Generate new points for the smoothed path
-    #     t_new = np.linspace(0, len(x_points) - 1, num_points)
-    #     smooth_x = cs_x(t_new)
-    #     smooth_y = cs_y(t_new)
+        # Generate new points for the smoothed path
+        t_new = np.linspace(0, len(x_points) - 1, num_points)
+        smooth_x = cs_x(t_new)
+        smooth_y = cs_y(t_new)
         
-    #     # Reconstruct the smoothed path as nav_msgs/Path
-    #     smoothed_path = Path()
-    #     smoothed_path.header = input_path.header  # Copy over the header
+        # Reconstruct the smoothed path as nav_msgs/Path
+        smoothed_path = Path()
+        smoothed_path.header = input_path.header  # Copy over the header
         
-    #     for i in range(num_points):
-    #         pose = PoseStamped()
-    #         pose.pose.position.x = smooth_x[i]
-    #         pose.pose.position.y = smooth_y[i]
-    #         pose.pose.position.z = 0.0  # Assuming 2D navigation; set z to 0
+        for i in range(num_points):
+            pose = PoseStamped()
+            pose.pose.position.x = smooth_x[i]
+            pose.pose.position.y = smooth_y[i]
+            pose.pose.position.z = 0.0  # Assuming 2D navigation; set z to 0
             
-    #         # Optionally set orientation (you could calculate it based on the path direction)
-    #         pose.pose.orientation.w = 1.0  # Default orientation (unit quaternion)
+            # Optionally set orientation (you could calculate it based on the path direction)
+            pose.pose.orientation.w = 1.0  # Default orientation (unit quaternion)
             
-    #         smoothed_path.poses.append(pose)
+            smoothed_path.poses.append(pose)
         
-    #     return smoothed_path
-
-    # def check_goal_reached(self):
-    #     # Get the last waypoint in the smoothed path
-    #     last_waypoint = self.smoothed_path.poses[-1]  # Last waypoint
-
-    #     # Calculate distance to the last waypoint
-    #     last_x = last_waypoint.pose.position.x
-    #     last_y = last_waypoint.pose.position.y
-    #     current_x = self.current_pose.pose.position.x
-    #     current_y = self.current_pose.pose.position.y
-
-    #     distance_to_goal = math.hypot(last_x - current_x, last_y - current_y)
-    #     self.get_logger().info(f"Distance from current position to goal: {distance_to_goal}")
-
-    #     # Check if the robot has reached the last waypoint (goal)
-    #     if distance_to_goal < 0.1:  # Example threshold (10 cm)
-    #         self.goal_achieved = True
-    #         self.smoothed_path = None
-    #         self.get_logger().info("Reached the goal.")
-
-    # def check_target_pose_reached(self, current_pose, target_pose, threshold=1e-3):
-    #     # Calculate distance to the target pose from the current pose
-    #     distance_to_target = math.hypot(target_pose.pose.position.x - current_pose.pose.position.x, target_pose.pose.position.y - current_pose.pose.position.y, target_pose.pose.position.z - current_pose.pose.position.z)
-    #     self.get_logger().info(f"Distance from current position to target: {distance_to_target}")
-
-    #     # Check if the robot has reached the last target pose
-    #     if distance_to_target < threshold:
-    #         self.get_logger().info(f"Reached the target {target_pose} with threshold {threshold}")
-    #         return True
-        
-    #     return False
-
-    def check_if_rotate_needed(self, current_pose, target_pose):
-        yaw_rotation = controller.compute_required_yaw_rotation(current_pose, target_pose)
-        self.get_logger().info(f"Yaw rotation: {yaw_rotation}")
-        if abs(yaw_rotation) > self.threshold_angular:
-            return True
-        
-        return False
+        return smoothed_path
 
 def main(args=None):
     rclpy.init(args=args)
